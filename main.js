f9d87737e5e5878331338a108660623871ae3921
Fresh-Teacher <freshteacheruganda256@gmail.com>
2023-07-18 13:45:18 -0700

    add search bar

--- main.js
+++ main.js
@@ -56,7 +56,6 @@ fetch('movies.json')
         let imga = document.createElement("img");
         let description = document.createElement("p");
         let title = document.createElement("h3");
-        let imd = document.createElement("div");
         imga.id = "popupimg";
         imga.src = data[i].thumbnail;
         imga.height = 150;
@@ -155,6 +154,240 @@ fetch('movies.json')
         popup.showModal();
       });
     }
+  
+  // Declare filteredMovies outside the fetch block
+  let filteredMovies = [];
+
+// Function to search movies based on user input
+function searchMovies() {
+  const searchInput = document.getElementById("searchInput").value.trim().toLowerCase();
+
+  // Fetch movies data from movies.json
+  fetch("movies.json")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      // Filter the movies based on the search input
+      filteredMovies = data.filter((movie) => {
+        return movie.title.toLowerCase().includes(searchInput);
+      });
+
+      // Clear the existing movie display
+      const filmContainer = document.getElementById("film");
+      filmContainer.innerHTML = "";
+
+      // If no movies match the search criteria, show a message
+      if (filteredMovies.length === 0) {
+        const noResultsMessage = document.createElement("p");
+        noResultsMessage.textContent = "No movies found.";
+        filmContainer.appendChild(noResultsMessage);
+      } else {
+        // Generate the HTML elements for the filtered movies
+        filteredMovies.forEach((movie, i) => {
+          // Create the container div for each movie
+          let movieContainer = document.createElement("div");
+          movieContainer.classList.add("display", "col-md-4");
+
+          // Create the movie thumbnail
+          let thumbnail = document.createElement("img");
+          thumbnail.id = `imag${i}`;
+          thumbnail.src = movie.thumbnail;
+          thumbnail.width = 200;
+          thumbnail.height = 250;
+
+          // Add a hover event listener to the thumbnail
+          thumbnail.addEventListener("mouseover", () => {
+            thumbnail.style.opacity = 0.8;
+          });
+
+          // Add a click event listener to the thumbnail
+          thumbnail.addEventListener("click", () => {
+            popupShowModal(movie);
+          });
+
+          // Append the thumbnail to the movie container
+          movieContainer.appendChild(thumbnail);
+
+          // Create the div for movie information
+          let info = document.createElement("div");
+          info.classList.add("info");
+
+          // Create a heading for the movie title
+          let titleHeading = document.createElement("h2");
+          titleHeading.textContent = movie.title;
+
+          // Add a click event listener to the movie title
+          titleHeading.addEventListener("click", () => {
+            popupShowModal(movie);
+          });
+
+          // Create a paragraph for the movie description
+          let descriptionParagraph = document.createElement("p");
+          descriptionParagraph.textContent = movie.description;
+
+          // Append the title and description to the info div
+          info.appendChild(titleHeading);
+          info.appendChild(descriptionParagraph);
+
+          // Append the info div to the movie container
+          movieContainer.appendChild(info);
+
+          // Create the div for movie status
+          let statusDiv = document.createElement("div");
+          statusDiv.classList.add("movie-status");
+
+          // Check if the movie is translated or untranslated
+          if (!movie.href || !movie.href.endsWith(".mp4")) {
+            statusDiv.innerHTML = "Untranslated";
+          } else {
+            statusDiv.innerHTML = "Translated";
+          }
+
+          // Append the status div to the movie container
+          movieContainer.appendChild(statusDiv);
+
+          // Append the movie container to the film container
+          filmContainer.appendChild(movieContainer);
+        });
+      }
+    })
+    .catch((err) => {
+      console.log(`Error: ${err}`);
+    });
+}
+
+// Event listener for the search input
+document.getElementById("searchInput").addEventListener("input", searchMovies);
+
+// Function to scroll to the top of the page
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
+
+// Show/hide the scroll-to-top button based on scroll position
+window.addEventListener("scroll", function () {
+  var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+  if (window.pageYOffset > 300) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+});
+
+  // Function to open a modal with movie details
+  function popupShowModal(movie) {
+    // Create the modal with movie details
+    let popup = document.createElement("dialog");
+    popup.id = "pop";
+  
+    let div3 = document.createElement("div");
+    let imga = document.createElement("img");
+    let description = document.createElement("p");
+    let title = document.createElement("h3");
+    imga.id = "popupimg";
+    imga.src = movie.thumbnail;
+    imga.height = 150;
+    imga.width = 100;
+  
+    description.style.color = "white";
+    description.innerHTML = `${movie.extract}`;
+  
+    title.style.color = "white";
+    title.innerHTML = movie.title;
+  
+    let titleDiv = document.createElement("div");
+    titleDiv.appendChild(title);
+  
+    let cast = document.createElement("p");
+    cast.classList.add("movie-cast");
+    cast.innerHTML = `Cast: ${movie.cast.join(", ")}`;
+  
+    let genreDiv = document.createElement("div");
+    let genres = document.createElement("p");
+    genres.classList.add("movie-genre");
+    genres.innerHTML = `Genres: ${movie.genres.join(", ")}`;
+    genreDiv.appendChild(genres);
+  
+    let overviewDiv = document.createElement("div");
+    overviewDiv.appendChild(description);
+  
+    div3.appendChild(titleDiv);
+    div3.appendChild(overviewDiv);
+    div3.appendChild(genreDiv);
+    div3.appendChild(cast);
+  
+    let buttonsContainer = document.createElement("div");
+    buttonsContainer.style.marginTop = "10px"; // Add top margin for spacing
+  
+    let video = document.createElement("video");
+    video.src = movie.href;
+    video.controls = true;
+    video.style.width = "100%";
+    video.style.display = "none";
+    div3.appendChild(video);
+  
+    if (!movie.href || !movie.href.endsWith(".mp4")) {
+      let untranslatedText = document.createElement("p");
+      untranslatedText.textContent =
+        "The translated version of this movie is not yet available but it will be available soon. At the moment, you may watch the untranslated movie.";
+      untranslatedText.style.fontFamily = "Arial, sans-serif";
+      untranslatedText.style.fontSize = "18px";
+      untranslatedText.style.fontWeight = "bold";
+      untranslatedText.style.fontStyle = "italic";
+      untranslatedText.style.marginBottom = "10px";
+      untranslatedText.style.color = "red";
+      div3.appendChild(untranslatedText);
+  
+      let watchUntranslatedButton = document.createElement("button");
+      watchUntranslatedButton.textContent = "Watch Untranslated Movie ▷";
+      watchUntranslatedButton.classList.add("btn", "btn-secondary");
+      watchUntranslatedButton.addEventListener("click", () => {
+        window.location.href = "https://soaps.vercel.app";
+      });
+      buttonsContainer.appendChild(watchUntranslatedButton);
+    } else {
+      let watchButton = document.createElement("button");
+      watchButton.textContent = "Watch Movie ▶️";
+      watchButton.classList.add("btn", "btn-success");
+      watchButton.style.marginRight = "10px"; // Add right margin for spacing
+      watchButton.addEventListener("click", () => {
+        video.style.display = "block";
+      });
+      buttonsContainer.appendChild(watchButton);
+  
+      let downloadButton = document.createElement("button");
+      downloadButton.textContent = "Download Movie 📥";
+      downloadButton.classList.add("btn", "btn-primary");
+      downloadButton.addEventListener("click", () => {
+        window.location.href = movie.href;
+      });
+      buttonsContainer.appendChild(downloadButton);
+    }
+  
+    div3.appendChild(buttonsContainer);
+  
+    let closeButton = document.createElement("button");
+    closeButton.classList.add("btn", "btn-danger");
+    closeButton.textContent = "Close ❌";
+    closeButton.style.marginTop = "10px";
+    closeButton.addEventListener("click", () => {
+      popup.close();
+    });
+  
+    div3.appendChild(closeButton);
+  
+    popup.appendChild(imga);
+    popup.appendChild(div3);
+  
+    document.getElementsByTagName("body")[0].appendChild(popup);
+  
+    popup.showModal();
+  }  
+
 
     
     // Pagination
